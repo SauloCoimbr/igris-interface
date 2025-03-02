41800bd9bebed970746901cca1d5b0e849115b46
Saulo Cavalcante Coimbra <121812117+SauloCoimbr@users.noreply.github.com>
2025-03-01 22:11:31 -0300

    enhancement icons and coding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,6 @@ import {
 
 export type IconProps = React.HTMLAttributes<SVGElement>;
 
-// Tipo para a janela aberta (com um id único)
 interface WindowData {
   url: string;
   key: string;
@@ -32,7 +31,6 @@ export default function Home() {
     return () => clearInterval(timer);
   }, []);
 
-  // Abre uma nova janela com um key único
   const openSite = (url: string) => {
     setOpenWindows([...openWindows, { url, key: `${url}-${Date.now()}` }]);
   };
@@ -207,7 +205,7 @@ export default function Home() {
             className="bg-white rounded-lg shadow-xl overflow-hidden mb-4"
           >
             {/* Header da janela com a área de arrasto */}
-            <div className="drag-handle bg-gray-200 flex items-center p-2 cursor-move">
+            <div className="drag-handle bg-gray-200 flex items-center p-2 cursor-default">
               <div className="flex space-x-2 ml-2">
                 <div
                   className="w-3 h-3 bg-red-500 rounded-full cursor-pointer"
@@ -350,7 +348,7 @@ const Icons = {
       <path
         fill="#fff"
         d="m12.966 161.238 10.439-38.114a73.42 73.42 0 0 1-9.821-36.772c.017-40.556 33.021-73.55 73.578-73.55 19.681.01 38.154 7.669 52.047 21.572s21.537 32.383 21.53 52.037c-.018 40.553-33.027 73.553-73.578 73.553h-.032c-12.313-.005-24.412-3.094-35.159-8.954z"
-        />
+      />
       <path
         fill="url(#b)"
         d="M87.184 25.227c-33.733 0-61.166 27.423-61.178 61.13a60.98 60.98 0 0 0 9.349 32.535l1.455 2.313-6.179 22.558 23.146-6.069 2.235 1.324c9.387 5.571 20.15 8.517 31.126 8.523h.023c33.707 0 61.14-27.426 61.153-61.135a60.75 60.75 0 0 0-17.895-43.251 60.75 60.75 0 0 0-43.235-17.928z"
